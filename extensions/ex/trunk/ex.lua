1fa1755843e10a25ff66dbdfa81751d93e6f9373
rpusztai <rpusztai@7ee9cf8c-424f-0410-93fe-4772f0734599>
2008-08-06 00:19:56 +0000

    Updated the Premake script a bit.

--- extensions/ex/trunk/ex.lua
+++ extensions/ex/trunk/ex.lua
@@ -5,19 +5,20 @@ addoption( "ex-lua-src",   "Path to the Lua src directory. (default: ../lua)" )
 
 -- GENERAL SETUP -------------------------------------------------------------
 --
-package.name							= "ex"
-package.language						= "c"
+package.name								= "ex"
+package.language							= "c"
 if options["ex-shared"] then
-	package.kind						= "dll"
+	package.kind							= "dll"
 else
-	package.kind						= "lib"
+	package.kind							= "lib"
 end
-package.target							= package.name
-package.config["Debug"].target			= package.target .. "d"
+package.target								= package.name
+package.config["Debug"].target				= package.target .. "d"
 
 -- COMPILER SETTINGS ----------------------------------------------------------
 --
 -- Build Flags
+package.buildflags							= { "no-import-lib" }
 if options["dynamic-runtime"] then
 	package.config["Release"].buildflags	= { "no-symbols", "optimize" }
 else
@@ -25,40 +26,31 @@ else
 	package.config["Release"].buildflags	= { "static-runtime", "no-symbols", "optimize" }
 end
 
-if options["ex-shared"] then
-	if ( target == "gnu" ) or ( string.find( target or "", ".*-gcc" ) ) then
-		table.insert( package.buildflags, { "no-import-lib" } )
-	end
-end
-
 -- Defined Symbols
-package.config["Debug"].defines			= { "DEBUG", "_DEBUG" }
-package.config["Release"].defines		= { "NDEBUG" }
-
--- Set the objects directories.
-package.objdir							= ".obj"
+package.config["Debug"].defines				= { "DEBUG", "_DEBUG" }
+package.config["Release"].defines			= { "NDEBUG" }
 
 -- Files
 if windows then
-	package.files						= { matchrecursive( "w32api/*.h", "w32api/*.c" ) }
+	package.files							= { matchrecursive( "w32api/*.h", "w32api/*.c" ) }
 else
-	package.files						= { "posix/ex.c", "posix/ex.h", "posix/spawn.c", "posix/spawn.h" }
+	package.files							= { "posix/ex.c", "posix/ex.h", "posix/spawn.c", "posix/spawn.h" }
 end
 
 -- Include paths
-package.includepaths					= { ( options["ex-lua-src"] or "../lua" ) }
+package.includepaths						= { ( options["ex-lua-src"] or "../lua" ) }
 
 -- LINKER SETTINGS ------------------------------------------------------------
 --
 -- Libraries to link against.
 if options["ex-shared"] then
-	package.links						= { "LuaLib" }
+	package.links							= { "LuaLib" }
 end
 
 -- Linker directory paths.
 -- all files are included in one directory
 if options["ex-shared"] then
-	package.libpaths					= { "lib", "../lib" }
+	package.libpaths						= { "lib", "../lib" }
 end
 
 -- COMPILER SPECIFIC SETUP ----------------------------------------------------
@@ -66,11 +58,21 @@ end
 if ( target == "gnu" ) or ( string.find( target or "", ".*-gcc" ) ) then
 	table.insert( package.buildflags, { "extra-warnings", "no-import-lib" } )
 	table.insert( package.buildoptions, { "-W" } )
+	-- Set object output directory.
+	if options["unicode"] then
+		package.objdir						= ".obju"
+	else
+		package.objdir						= ".obj"
+	end
 end
 
 if ( target == "vs2005" ) or ( target == "vs2008" ) then
 	table.insert( package.defines, { "_CRT_SECURE_NO_DEPRECATE" } )
 	table.insert( package.linkoptions, { "/DEF:ex.def" } )
+	-- Set object output directory.
+	if options["unicode"] then
+		package.objdir						= "obju"
+	end
 end
 
 -- OPERATING SYSTEM SPECIFIC SETTINGS -----------------------------------------
